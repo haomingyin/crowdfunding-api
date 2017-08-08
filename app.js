834fb34acc9b07ffd66c9146ee65c0d2bc774748
haoming <haoming.yin@telogis.com>
2017-08-09 10:44:11 +1200

    The current setting works for local machine's docker

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require('./config/express');
 const app = express();
 
 // const PORT = process.env.SENG365_PORT;
-const PORT = 80;
+const PORT = 4941;
 
 // connect to MySQL on start
 // db.connect(function (err) {
