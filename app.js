574b7f0103352aee7009ddab2582a3a42c5f1a57
Haoming Yin <hyi25@uclive.ac.nz>
2017-08-09 18:58:17 +1200

    Added projects, rewards and users controllers
    Added sql initialization file

--- app.js
+++ app.js
@@ -9,33 +9,33 @@ const app = express();
 const PORT = 4941;
 
 // connect to MySQL on start
-// setTimeout(function () {
-//     db.connect(function (err) {
-//         if (err) {
-//             console.log("Unable to connect to MySQL.");
-//         } else {
-//             app.listen(PORT, function () {
-//                 console.log('Listening on port: ' + PORT);
-//             })
-//         }
-//     });
-// }, 5000);
-
-// set a time out to wait for mysql to be initialized
 setTimeout(function () {
-    db.initialize(function (err) {
+    db.connect(function (err) {
         if (err) {
-            console.log("Unable to initialize MySQL.");
+            console.log("Unable to connect to MySQL.");
         } else {
             app.listen(PORT, function () {
                 console.log('Listening on port: ' + PORT);
-            });
+            })
         }
     });
 }, 5000);
 
+// set a time out to wait for mysql to be initialized
+// setTimeout(function () {
+//     db.initialize(function (err) {
+//         if (err) {
+//             console.log("Unable to initialize MySQL.");
+//         } else {
+//             app.listen(PORT, function () {
+//                 console.log('Listening on port: ' + PORT);
+//             });
+//         }
+//     });
+// }, 5000);
+
 app.get("/hi", function (req, res) {
-    db.getPool().query("SELECT COUNT(*) FROM users;", function (err, result) {
+    db.getPool().query("SELECT COUNT(*) FROM user;", function (err, result) {
         if (err) {
             res.send(err);
         } else {
